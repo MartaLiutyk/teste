8d63e8055f252e5e0ac6f4d94752ea0fdf5c24c8
MartaLiutyk <melodika99@gmail.com>
2016-11-04 18:10:30 -0200

    Update vigenere.c

--- projects/vigenere.c
+++ projects/vigenere.c
@@ -10,58 +10,57 @@
 #define END_OF_BIG_LETTERS 90
 #define END_OF_SMALL_LETTERS 122
 
-
 int main(int argc, string argv[]) 
 {
-string key = argv[1];
-  //перевірка чи працює коректно
-if (argc !=2)
-      {
-       printf("Please, try again");
-       return 1;
-       }
-  
-  //перевірка чи немає символів замість літер
-for (int i = 0; i < strlen(key); i++)
-   {
-   if (!isalpha(key[i]))
-   {
-   printf("Keyword has unaccetable symbols. Please, try again.");
-   return 1;
-   }
- }
-  //введення  тексту, який необхідно зашифрувати
- string TextToChange;
- TextToChange = GetString();
-  
-  char keyLengthOfText[strlen(TextToChange)];
+	string key = argv[1];
+	//перевірка чи працює коректно
+	if (argc !=2)
+	{
+		printf("Please, try again");
+		return 1;
+	}
 
-for ( int i = 0, j = 0; i < strlen(TextToChange); i++)
-    {
-            //перевірка , чи серед введених символів є щось окрім літер
-      if (((TextToChange[i] >= CAPITAL_LETTER_A) && (TextToChange[i] <= END_OF_BIG_LETTERS))
-      || ((TextToChange[i] >= SMALL_LETTER_A) && (TextToChange[i] <= END_OF_SMALL_LETTERS)))
-    {
-  //заповнення "стрічки" ключовим  словом
-keyLengthOfText[i] = key[j % strlen(key)];
-j++;
-}
+	//перевірка чи немає символів замість літер
+	for (int i = 0; i < strlen(key); i++)
+	{
+		if (!isalpha(key[i]))
+		{
+			printf("Keyword has unaccetable symbols. Please, try again.");
+			return 1;
+		}
+	}
+	//введення тексту, який необхідно зашифрувати
+	string TextToChange;
+	TextToChange = GetString();
+
+	char keyLengthOfText[strlen(TextToChange)];
+
+	for ( int i = 0, j = 0; i < strlen(TextToChange); i++)
+	{
+		//перевірка , чи серед введених символів є щось окрім літер
+		if (((TextToChange[i] >= CAPITAL_LETTER_A) && (TextToChange[i] <= END_OF_BIG_LETTERS)) || ((TextToChange[i] >= SMALL_LETTER_A) && (TextToChange[i] <= END_OF_SMALL_LETTERS)))
+		{
+			//заповнення "стрічки" ключовим словом
+			keyLengthOfText[i] = key[j % strlen(key)];
+			j++;
+		}
+	}
+
+	//Велику літеру змінюємо на велику
+	for (int i = 0; i < strlen(TextToChange); i++)
+	{
+		if ((TextToChange[i] >= CAPITAL_LETTER_A) && (TextToChange[i] <= END_OF_BIG_LETTERS))
+		{
+			TextToChange[i] = CAPITAL_LETTER_A + (TextToChange[i] - CAPITAL_LETTER_A + toupper(keyLengthOfText[i]) - CAPITAL_LETTER_A)%LETTERS_NUMBER;
+		}
+		//якщо літера мала , змінюємо на малу
+		else if ((TextToChange[i] >= SMALL_LETTER_A) && (TextToChange[i] <= END_OF_SMALL_LETTERS))
+		{
+			TextToChange[i] = SMALL_LETTER_A + (TextToChange[i] - SMALL_LETTER_A + tolower(keyLengthOfText[i]) - SMALL_LETTER_A)%LETTERS_NUMBER;
+		}
+	}
+	//виведення результату роботи програми
+	printf("%s\n", TextToChange);
+	return 0;
 }
 
-        //Велику  літеру змінюємо на велику
- for (int i = 0; i < strlen(TextToChange); i++)
-         {
-         if ((TextToChange[i] >= CAPITAL_LETTER_A) && (TextToChange[i] <= END_OF_BIG_LETTERS))
-            {
-              TextToChange[i] = CAPITAL_LETTER_A + (TextToChange[i] - CAPITAL_LETTER_A + toupper(keyLengthOfText[i]) - CAPITAL_LETTER_A)%LETTERS_NUMBER;
-            }
-       //якщо літера  мала , змінюємо на малу
-         else if ((TextToChange[i] >= SMALL_LETTER_A) && (TextToChange[i] <= END_OF_SMALL_LETTERS))
-            {
-            TextToChange[i] = SMALL_LETTER_A + (TextToChange[i] - SMALL_LETTER_A + tolower(keyLengthOfText[i]) - SMALL_LETTER_A)%LETTERS_NUMBER;
-            }
-      }
-  //виведення результату роботи програми
- printf("%s\n", TextToChange);
-return 0;
-}
